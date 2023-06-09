d21b03b00829b2b308f5d402adc7357aec9430f2
piouskenny <kehindeu13@gmail.com>
2023-06-09 02:00:45 +0100

    added the middleware functionality

--- app/Http/Middleware/AdminAuthMiddleWare.php
+++ app/Http/Middleware/AdminAuthMiddleWare.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Admin;
 use Closure;
 use Illuminate\Http\Request;
 
@@ -16,6 +17,12 @@ class AdminAuthMiddleWare
      */
     public function handle(Request $request, Closure $next)
     {
+        if (!session()->has('LoggedUser')) {
+            return redirect('/bl-admin/login');
+        } elseif (session()->has('LoggedUser')) {
+            $user = Admin::where('id', session('LoggedUser'))->first();
+        }
+
         return $next($request);
     }
 }
